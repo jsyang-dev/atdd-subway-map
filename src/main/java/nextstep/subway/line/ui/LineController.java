437fd188eb34566ee92bade1f8243a7c30df32aa
Jeongsu Yang <mycat83@gmail.com>
2021-03-05 22:29:17 +0900

    feat: 지하철 노선 제거

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -65,4 +65,10 @@ public class LineController {
         lineService.updateLine(id, lineRequest);
         return ResponseEntity.ok().build();
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
+        lineService.deleteLine(id);
+        return ResponseEntity.noContent().build();
+    }
 }
